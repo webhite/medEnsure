8131f6a32edbfb7fd25cca338d082a82457fcdbd
Parvej Khan <1999monustp@gmail.com>
2026-02-03 02:45:50 +0530

    initial commit

---
+++ med_ensure/local_settings.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "med_ensure",
+        'USER': "parvej",
+        'PASSWORD': "parvej",
+        'HOST': "localhost"
+    }
+}
